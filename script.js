e684854421d4e45e0b528b6575e1cff0d1126e57
kersiancoder <maxi.heijo@gmail.com>
2022-08-26 10:55:48 -0300

    final

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let veces = prompt("Ingrese un número del 1 al 10 (ESC para salir)")
-let texto = "Hola"
+let veces = prompt("Ingrese un número (ESC para salir)")
+let texto = "Desafío 01 Maximiliano Heijo"
 
-for(let i=1;i<=veces;i++){
-alert(texto + " se repetirá " + veces + " veces.")
+while (!/^[0-9]+$/.test(veces)) {
+    alert("No ingresaste un número válido.")
+    veces = prompt("Ingrese un número (ESC para salir)")
+}
+for(let i=1;i<=veces;i){
+    while ((veces > 1 && veces != "ESC")) {
+        alert(texto + " se repetirá " + veces-- + " veces.")
+    }
+alert(texto + " se repetirá " + veces-- + " vez.")
 }
