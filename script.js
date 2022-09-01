1e4bcd749a37025d734d3613d72ee968e36201a4
kersiancoder <maxi.heijo@gmail.com>
2022-09-01 09:50:05 -0300

    desafio 1

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-let veces = prompt("Ingrese un número")
-let texto = "Desafío 01 Maximiliano Heijo"
 
-while (!/^[0-9]+$/.test(veces)) {
-    alert("No ingresaste un número válido.")
-    veces = prompt("Ingrese un número")
-}
-for(let i=1;i<=veces;i){
-    while (veces > 1) {
-        alert(texto + " se repetirá " + veces-- + " veces.")
+
+
+function repetidorTexto() {
+    let veces = prompt("Ingrese un número del 1 al 50")
+    let texto = "Desafío 01 Maximiliano Heijo"
+   /*  while (!/^[0-9]+$/.test(veces)) {
+        alert("No ingresaste un número válido.")
+        veces = prompt("Ingrese un número")
+    } */
+    
+    if (veces => 50) {
+        console.log(veces)
+        alert("Ingrese un número menor de 50")
+        veces = prompt("Ingrese un número del 1 al 50")
     }
-alert(texto + " se repetirá " + veces-- + " vez.")
+    else {
+        console.log("test")
+        for(let i=1;i<=veces;i){
+            while (veces > 1) {
+                alert(texto + " se repetirá " + veces-- + " veces.")
+            }
+            alert(texto + " se repetirá " + veces-- + " vez.")
+        }
+    }
 }
+
+repetidorTexto()
